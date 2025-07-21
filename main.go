9df2858f6d4a455701aa5a0f1a66ca4060c7da78
jkuya <kuyajesee@proton.me>
2025-07-21 23:03:50 +0300

    chore: format import statements for improved readability

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jesee-kuya/blue/handler"
+)
 
 func main() {
-	r := gin.Default()	
+	r := gin.Default()
 
 	r.GET("/health", handler.HealthCheck)
 	r.GET("/search", handler.SearchHandler)
