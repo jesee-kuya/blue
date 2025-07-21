533dbffea71eb8b2cbf9f592b8e03317ed5dfd09
jesee-kuya <kuyajesee@proton.me>
2025-07-22 00:58:55 +0300

    refacto: add test support to vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults } from 'vitest/config'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    exclude: [...configDefaults.exclude],
+    coverage: {
+      reporter: ['json-summary', 'text', 'lcov'], // JSON summary is needed
+    },
+  },
 })
