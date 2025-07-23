afcf087cb5a726f4395279c954753265969eed84
jkuya <kuyajesee@proton.me>
2025-07-23 04:51:09 +0300

    feat: add the types

---
+++ internal/marketplace/types.go
@@ -0,0 +1,13 @@
+package marketplace
+
+// Product represents a standardized product from any marketplace
+type Product struct {
+    Title string  `json:"title"`
+    Price float64 `json:"price"`
+    Link  string  `json:"link"`
+}
+
+// Client defines the interface that all marketplace clients must implement
+type Client interface {
+    Search(query string, minPrice, maxPrice float64) ([]Product, error)
+}
